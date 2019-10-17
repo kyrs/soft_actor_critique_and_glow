1a6b1d3bf7f80b1c54249af970751a13b6ae14d8
kyrs <kumarshubham652@gmail.com>
2019-10-18 01:05:55 +0530

    new changes for the model code

--- src/soft_actor_critique/model.py
+++ src/soft_actor_critique/model.py
@@ -9,22 +9,31 @@ written in Tf2.0
 
 import tensorflow as tf
 
-class Policy(object):
-	def __init__(self,eps_layers=6,conv_size = 1):
-		## total no of layers in a network
-		self.eps_layers = eps_layers 
-		self.eps_name_size = {"enc_eps_0":(128, 128, 6),"enc_eps_1":(64, 64, 12),
+eps_name_size = {"enc_eps_0":(128, 128, 6),"enc_eps_1":(64, 64, 12),
 							"enc_eps_2":( 32, 32, 24),
 							"enc_eps_3":(16, 16, 48),
 							"enc_eps_4":(8, 8, 96),
 							"enc_eps_5": ( 4, 4, 384)}
 
+ordLayProcs = ["enc_eps_0","enc_eps_1","enc_eps_2","enc_eps_3","enc_eps_4","enc_eps_5"]
+
+#---------------defining the POlicy -------------------------
+class Policy(object):
+	def __init__(self,eps_layers=6,conv_size = 1):
+		## total no of layers in a network
+		self.eps_layers = eps_layers 
+		# self.eps_name_size = {"enc_eps_0":(128, 128, 6),"enc_eps_1":(64, 64, 12),
+							# "enc_eps_2":( 32, 32, 24),
+							# "enc_eps_3":(16, 16, 48),
+							# "enc_eps_4":(8, 8, 96),
+							# "enc_eps_5": ( 4, 4, 384)}
+
 		## creating a list of model based on the input data
 		self.modelDict = {name:self.fnApprox(layer_specific_mean=[(shape[2],1)],layer_specific_var=[(shape[2],1)],input_shape=shape) 
-							for name,shape in self.eps_name_size}
+							for name,shape in eps_name_size}
 
 
-	def fnApprox(self,layer_specific_mean=[],layer_specific_var=[],input_shape=[],batch_norm=True,dropout=False,drop_ratio=0.5 )
+	def fnApprox(self,layer_specific_mean=[],layer_specific_var=[],input_shape=[],batch_norm=True,dropout=False,drop_ratio=0.5 ):
 	"""
 	layer_specific_mean : list of tuple with (output layer specifics and kernel shapes)
 	layer_specific_var  : list of tuple with (output layer specifics and kernel shapes)
@@ -77,13 +86,14 @@ class Policy(object):
 
 
 
-	def sampNexState(self,modelDict={},inputDict={}):
+	def samplePolicy(self,modelDict={},inputDict={}):
 	"""
 	modelDict : dict of model for finding the mean and variance of an action
 	inputDict : dict of input state vector
 
 	---return---
-	return next state after taking an action
+	return next state, and action resulting it
+
 	"""
 		outputDict = {}
 
@@ -94,14 +104,112 @@ class Policy(object):
 			varLog  = varModel(value, Training=False)
 
 			newVal = tf.random.normal(meanMat.shape,mean=meanMat,stdDev =tf.sqrt(tf.exp(varLog)))
-			outputDict[layerName] = layerValue+newVal
+			outputDict[layerName]["new_state"] = layerValue+newVal
+			outputDict[layerName]["action"] = newVal
+			outputDict[layerName]["old_state"] = layerValue
 		
 		return outputDict
 
 	def trainNet(self,loss):
 		## function for training the models 
-class QValFn(object):
-	pass
+		pass
 
+
+#-----------------------------defining the Q-value --------------------------------------
+class QValFn(object):
+	## function for approximating the Q-value for a given state and action
+	def __init__(self):
+
+		self.modelDict = {name:self.fnApprox(layer_specific_param=[(shape[2],1)],input_shape=shape) 
+							for name,shape in eps_name_size}
+		pass
+
+	def fnApprox(self,layer_specific_param=[],input_shape=[],batch_norm=True,dropout=False,drop_ratio=0.5,denseLayer=512):
+		##  function approximator, returning a concatenated output for given state and action processed file
+		"""
+		layer_specif_param : tuple of output dim and weight dim in the function approximator 
+		input_shape : shape of the layers 
+		
+		-----Return-------
+		model which take input state and action and return a dense layer output 
+
+		"""
+
+
+		state = tf.keras.layers.Input(shape=input_shape)
+		action = tf.keras.layers.Input(shape=input_shape)
+
+		initializer = tf.random_normal_initializer(0.,0.02)
+
+		stateModel = tf.keras.Sequential()
+		actionModel = tf.keras.Sequential()
+
+		## model processing the state
+		for output_shape,kernel_size  in layer_specific_param:
+			stateModel.add(tf.keras.layers.Conv2D(output_shape, kernel_size, padding='same',
+							 kernel_initializer=initializer))
+			if batch_norm:
+				stateModel.add(tf.keras.layers.BatchNormalization())
+			if dropout:
+				stateModel.add(tf.keras.Dropout(drop_ratio))
+			stateModel.add(tf.keras.layers.ReLU())
+
+		## doing flattening
+		stateModel.add(tf.keras.layers.Flatten())
+		stateModel.add(tf.keras.layers.Dense(denseLayer))
+
+		stateModelOut = stateModel(state)
+
+		## model processing the action
+		for output_shape,kernel_size  in layer_specific_param:
+			actionModel.add(tf.keras.layers.Conv2D(output_shape, kernel_size, padding='same',
+							 kernel_initializer=initializer))
+			if batch_norm:
+				actionModel.add(tf.keras.layers.BatchNormalization())
+			if dropout:
+				actionModel.add(tf.keras.Dropout(drop_ratio))
+			actionModel.add(tf.keras.layers.ReLU())
+		
+		## doing flattening
+		actionModel.add(tf.keras.layers.Flatten())
+		actionModel.add(tf.keras.layers.Dense(denseLayer))
+
+		actionModelOut = actionModel(action)
+
+		## feature concatenation 
+		concat = tf.keras.layers.Concatenate()
+		output = concat([stateModelOut,actionModelOut])
+
+		fnApprox = tf.keras.Model(inputs=[state,action],outputs=output)
+		return fnApprox
+
+	def QvalueApprox(self,denseLayerInfo=[512,1]):
+		## creating final Qvalue model which concatenate all the individual output and return a single QValue 
+
+		"""
+		denseLayerInfo : information about the dense layer
+
+		--return--
+		"""
+		modelList = [self.fnApprox(layer_specific_param=[(shape[2],1)],input_shape= eps_name_size[name]) 
+							for name in ordLayProcs]
+		inputList= [(tf.keras.layers.Input(shape=eps_name_size[name]),tf.keras.layers.Input(shape=eps_name_size[name])) 
+						for name in ordLayProcs]
+
+		concatOut = []
+		for model,outTensor in zip(ordLayProcs,inputList):
+			state,action = outTensor
+			output = model([state,action])
+			concatOut.append(output)
+
+		concatOut = tf.keras.layers.Concatenate()(concatOut)
+		
+
+
+
+
+
+
+# ---------------------------defining the value function --------------------------------
 class ValFn(object):
 	pass
